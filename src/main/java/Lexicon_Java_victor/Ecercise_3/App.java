77db5df81bfb6a40c2e027fdae290c96a5587832
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 09:55:30 +0100

    Exercise 3

---
+++ src/main/java/Lexicon_Java_victor/Ecercise_3/App.java
@@ -0,0 +1,35 @@
+package Lexicon_Java_victor.Ecercise_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+    	
+    	int a = 45;
+    	int b = 11;
+    	
+    	int c = 12;
+    	int d = 4;
+    	
+    	int e = 24;
+    	int f = 6;
+    	
+    	int g = 55;
+    	int h = 12;
+    	
+    	
+    	
+      System.out.print(a + b + " " + c * d + " " + e / f + " " );												
+      System.out.println(g - h);
+      
+   
+    
+    
+    
+    
+    }
+}
